c89a98b551763acd71f575a729d500beead5e7f0
poqianggo <62617100+poqianggo@users.noreply.github.com>
2020-03-30 13:29:31 -0400

    Update startDockerLocal.sh

--- startDockerLocal.sh
+++ startDockerLocal.sh
@@ -1,3 +1,3 @@
 docker-compose -f docker-compose.yml up --detach 
-sleep 5
-docker exec avdapp_geth_1 /bin/bash /dnet/node-setup.sh
+#sleep 5
+#docker exec avdapp_geth_1 /bin/bash /dnet/node-setup.sh
